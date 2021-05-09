4dcec79078c88c175c3ca8ba8bef45873cd0081c
kunal-visoulia <kunalvisoulia@gmail.com>
2021-05-09 21:30:27 +0530

    refactored into smart and dumb componenets

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,50 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-
-import { getcurrentProduct, getError, getProducts, getshowProductCode, State } from 'src/app/products/state/product.reducer';
-
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../product';
-import * as ProductActions from '../state/product.action';
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle = 'Products';
 
-  products$: Observable<Product[]>;
-  selectedProduct$: Observable<Product>;
-  displayCode$: Observable<boolean>;
-  errorMessage$: Observable<string>;
-
-  constructor( private store: Store<State>) { }//we want the global state here so we can accer,say, user state
-  //also we want the extended state in product reducer not global as global state does not contain products slice of state
-
-  ngOnInit(): void {
-    this.products$ = this.store.select(getProducts);
-    
-    this.errorMessage$ = this.store.select(getError);
-
-    this.store.dispatch(ProductActions.loadProducts());
-
-    this.selectedProduct$ = this.store.select(getcurrentProduct);//gets the currently selected product from store and highlight the product
-
-    this.displayCode$ = this.store.select(getshowProductCode);
-  }
+  //input is not getting observable but direct values
+  @Input() errorMessage: string;
+  @Input() products: Product[];
+  @Input() displayCode: boolean;
+  @Input() selectedProduct: Product;
+  @Output() displayCodeChanged = new EventEmitter<void>();
+  @Output() initializeNewProduct = new EventEmitter<void>();
+  @Output() productWasSelected = new EventEmitter<Product>();
 
   checkChanged(): void {
-    this.store.dispatch(ProductActions.toggleProductCode());
+    this.displayCodeChanged.emit();
   }
 
   newProduct(): void {
-    this.store.dispatch(ProductActions.initializeCurrentProduct());
+    this.initializeNewProduct.emit();
   }
 
   productSelected(product: Product): void {
-    this.store.dispatch(ProductActions.setCurrentProduct({ currentProductId : product.id })); //setcurrent product action and retian the current product in the store
+    this.productWasSelected.emit(product);
   }
 
 }
--- src/app/products/product-shell/product-shell.component.ts
+++ src/app/products/product-shell/product-shell.component.ts
@@ -1,12 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { getcurrentProduct, getError, getProducts, getshowProductCode, State } from 'src/app/products/state/product.reducer';
+
+import { Product } from '../product';
+import * as ProductActions from '../state/product.action';
 
 @Component({
   templateUrl: './product-shell.component.html'
 })
 export class ProductShellComponent implements OnInit {
 
-  constructor() { }
+ 
+  products$: Observable<Product[]>;
+  selectedProduct$: Observable<Product>;
+  displayCode$: Observable<boolean>;
+  errorMessage$: Observable<string>;
 
-  ngOnInit() {
+  constructor( private store: Store<State>) { }//we want the global state here so we can accer,say, user state
+
+  ngOnInit(): void {
+    this.store.dispatch(ProductActions.loadProducts());
+    this.products$ = this.store.select(getProducts);
+    this.errorMessage$ = this.store.select(getError);
+    this.selectedProduct$ = this.store.select(getcurrentProduct);//gets the currently selected product from store and highlight the product
+    this.displayCode$ = this.store.select(getshowProductCode);
+  }
+
+  checkChanged(): void {
+    this.store.dispatch(ProductActions.toggleProductCode());
+  }
+
+  newProduct(): void {
+    this.store.dispatch(ProductActions.initializeCurrentProduct());
+  }
+
+  productSelected(product: Product): void {
+    this.store.dispatch(ProductActions.setCurrentProduct({ currentProductId : product.id })); //setcurrent product action and retian the current product in the store
   }
 }
